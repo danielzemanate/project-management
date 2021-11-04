f1361c3dd88aa0644eff0eb80299c28adbf65be0
danielzemanate <danielzemanate@gmail.com>
2021-11-04 14:09:11 -0500

    configuraciones iniciales, tailwind, jsconfig, routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import 'styles/styles.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from 'pages/Home';
+import Users from 'pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/users" element={<Users />}/>
+      </Routes>
+    </BrowserRouter>
+    // <div className="App">
+    //   <div className='bg-indigo-500'>
+    //     <p>HOLA MUNDO</p>
+    //   </div>
+    // </div>
   );
 }
 
---
+++ src/pages/Users.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Users = () => {
+    return (
+        <div>
+            ESTE ES EL ADMIN USUARIOS
+        </div>
+    )
+}
+
+export default Users

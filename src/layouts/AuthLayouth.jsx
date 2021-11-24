f6e7ccbed646fc60f5723eff72211c899b52281e
danielzemanate <danielzemanate@gmail.com>
2021-11-23 19:51:35 -0500

    bug responsive h-screen

--- src/layouts/AuthLayouth.jsx
+++ src/layouts/AuthLayouth.jsx
@@ -6,11 +6,12 @@ import { Link } from 'react-router-dom';
 
 const AuthLayout = () => {
   return (
-    <div className='flex flex-col md:flex-row flex-no-wrap h-screen p-3'>
-      <div className='flex flex-col w-full h-full'>
-      <Link to="/">
+    <div className='flex flex-col md:flex-row flex-no-wrap h-full p-3'>
+       <Link to="/">
         <i className="fas fa-home text-gray-900 cursor-pointer font-bold text-xl hover:text-red-900" />
       </Link>
+      <div className='flex flex-col w-full h-full'>
+     
         <div className='flex flex-col items-center justify-center w-full h-full overflow-y-scroll'>
           <Outlet />
         </div>

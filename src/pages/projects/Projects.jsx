44034fd80e764452f3f6ce3b448d3861b2104a26
MichelleDuque <michelled_1994@hotmail.com>
2021-11-30 15:09:40 -0500

    Se agrego el cuadro responsive en projects

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -71,6 +71,26 @@ const Projects = () => {
             })}
         </tbody>
       </table>
+                    {/* CARDS PARA PANTALLAS MOVILES */}
+                    <div className='divCardsTable'>
+        {data &&
+            data.Proyectos.map((u) => {
+          return (
+            <div className="card mb-3" key={u._id}>
+                <div className="card-body">
+                    <h4 className="text-3xl font-semibold my-2 text-red-900">{u.nombre}</h4>
+                    <p className="card-text"><b>Presupuesto:</b> {u.presupuesto}</p>
+                    <p className="card-text"><b>Fecha Inicio:</b> {u.fechaInicio}</p>
+                    <p className="card-text"><b>Fecha Fin:</b> {u.fechaFin}</p>
+                    <p className="card-text"><b>Fase:</b> {u.fase}</p>   
+                    <Link className='flex mt-2' to={`/admin/projects/editar/${u._id}`}>
+                      <button className='bg-red-700 text-white font-bold text-xl py-2 px-1 rounded-xl hover:bg-red-500 shadow-md my-1'>EDITAR</button>
+                    </Link> 
+                </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
     )
 }

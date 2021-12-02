7d4bbd8ed70e9f5399df876e0f2b8c23c774e885
danielzemanate <danielzemanate@gmail.com>
2021-12-01 20:26:27 -0500

    arreglo bug route landing admin

--- src/components/NavbarPrivateLayout.jsx
+++ src/components/NavbarPrivateLayout.jsx
@@ -42,11 +42,11 @@ const NavbarPrivateLayout = () => {
         {/* <SidebarResponsive/> */}
         {/* <NavbarBrand className="navbarBrand" href="/landingAdmin">
         <img width="80" className="px-1" src={logo} alt=""></img>ADMINISTRADOR</NavbarBrand> */}
-        <NavbarBrand className="navbar-Brand hidden md:flex lg:flex sm:hidden text-4xl mb-2 font-semibold leading-tight" href="landingAdmin">
+        <NavbarBrand className="navbar-Brand hidden md:flex lg:flex sm:hidden text-4xl mb-2 font-semibold leading-tight" href="/admin/landingAdmin">
             {/* <img width="90" className="px-2 d-inline-block " src={logo} alt=""/> */}
             <h3 className='text-white shadow-md m-4'>GESTIÓN DE PROYECTOS</h3>
         </NavbarBrand>
-        <NavbarBrand className="navbar-Brand flex md:hidden text-2xl mb-2 font-semibold leading-tight" href="landingAdmin">
+        <NavbarBrand className="navbar-Brand flex md:hidden text-2xl mb-2 font-semibold leading-tight" href="/admin/landingAdmin">
             {/* <img width="90" className="px-2 d-inline-block " src={logo} alt=""/> */}
             <h1 className='text-white'>GESTIÓN DE PROYECTOS</h1>
         </NavbarBrand>

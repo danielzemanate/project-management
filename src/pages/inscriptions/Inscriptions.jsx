8cc4045b845582cbae41aa895fdcb0a6bf72f0be
MichelleDuque <michelled_1994@hotmail.com>
2021-12-06 21:12:37 -0500

    se agrego el boton de rechazado en inscriptions y se agregaron campos en sus detalles

--- src/pages/inscriptions/Inscriptions.jsx
+++ src/pages/inscriptions/Inscriptions.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import PrivateRoute from 'components/PrivateRoute';
 import { GET_INSCRIPCIONES } from 'graphql/inscripciones/queries';
-import { APROBAR_INSCRIPCION } from 'graphql/inscripciones/mutaciones';
+import { APROBAR_INSCRIPCION, RECHAZAR_INSCRIPCION } from 'graphql/inscripciones/mutaciones';
 import ButtonLoading from 'components/ButtonLoading';
 import { toast } from 'react-toastify';
 import {
@@ -63,6 +63,9 @@ const Inscriptions = () => {
   
   const Inscripcion = ({ inscripcion, refetch }) => {
     const [aprobarInscripcion, { data, loading, error }] = useMutation(APROBAR_INSCRIPCION);
+    const [rechazarInscripcion, { data_rechazar, loading_rechazar, error_rechazar }] = useMutation(RECHAZAR_INSCRIPCION);
+    
+    
   
     useEffect(() => {
       if (data) {
@@ -70,12 +73,25 @@ const Inscriptions = () => {
         refetch();
       }
     }, [data]);
+
+    useEffect(() => {
+      if (data_rechazar) {
+        toast.success('Inscripcion rechazada con exito');
+        refetch();
+      }
+    }, [data_rechazar]);
   
     useEffect(() => {
       if (error) {
         toast.error('Error aprobando la inscripcion');
       }
     }, [error]);
+
+    useEffect(() => {
+      if (error_rechazar) {
+        toast.error('Error rechazando la inscripcion');
+      }
+    }, [error_rechazar]);
   
     const cambiarEstadoInscripcion = () => {
       aprobarInscripcion({
@@ -83,19 +99,38 @@ const Inscriptions = () => {
           aprobarInscripcionId: inscripcion._id,
         },
       });
+      rechazarInscripcion({
+        variables: {
+          rechazarInscripcionId: inscripcion._id,
+        },
+      });
     };
   
     return (
       <div className='bg-gray-900 text-gray-50 flex flex-col p-6 m-2 rounded-lg shadow-xl'>
-        <span>{inscripcion.proyecto.nombre}</span>
-        <span>{inscripcion.estudiante.nombre}</span>
-        <span>{inscripcion.estado}</span>
+        <span>Proyecto: {inscripcion.proyecto.nombre}</span>
+        <span>Estudiante: {inscripcion.estudiante.nombre}</span>
+        <span>Estado: {inscripcion.estado}</span>
+        <span>ID Inscripcion: {inscripcion._id}</span>
+        <span>ID Proyecto: {inscripcion.proyecto._id}</span>
+        <span>ID Estudiante: {inscripcion.estudiante._id}</span>
+        <span>Fecha: {inscripcion.fechaIngreso}</span>
         {inscripcion.estado === 'PENDIENTE' && (
           <ButtonLoading
             onClick={() => {
               cambiarEstadoInscripcion();
             }}
             text='Aprobar Inscripcion'
+            loading={loading}
+            disabled={false}
+          />
+        )}
+          {inscripcion.estado === 'PENDIENTE' && (
+          <ButtonLoading
+            onClick={() => {
+              cambiarEstadoInscripcion();
+            }}
+            text='Rechazar Inscripcion'
             loading={loading}
             disabled={false}
           />
